fa9b92c3e9959d469d5fcf039da28c995dc1d444
FarhanFardid <farhanfardid009@gmail.com>
2023-05-30 17:59:35 +0600

    recaptcha added to login and firebase config added

--- src/Pages/Authentication/Login.jsx
+++ src/Pages/Authentication/Login.jsx
@@ -2,8 +2,34 @@ import { Link } from "react-router-dom";
 import loginBanner from "../../assets/others/authentication.gif";
 import authImg from "../../assets/others/authentication1.png";
 import { FaGoogle,FaFacebook,FaGithub } from 'react-icons/fa';
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { useEffect, useRef, useState } from "react";
 
 const Login = () => {
+
+    const captchaRef = useRef(null);
+    const [disable,setDisable] = useState(true)
+    useEffect(()=>{
+        loadCaptchaEnginge(6);
+    },[])
+    const handleCaptcha = ()=>{
+        const user_captcha_value = captchaRef.current.value;
+        console.log(user_captcha_value);
+        if (validateCaptcha(user_captcha_value)){
+           setDisable(false)
+        }
+        else{
+            setDisable(true)
+        }
+         }
+         const handleLogin= event =>{
+            event.preventDefault();
+            const form = event.target;
+            const email = form.email.value;
+            const password = form.password.value;
+            console.log(email,password);
+
+         }
   return (
     <div>
       <div
@@ -23,7 +49,7 @@ const Login = () => {
             <img src={authImg} alt="Auth Img" className="w-full" />
           </div>
           <div className="card w-full  shadow-2xl bg-base-300 m-8">
-            <div className="card-body m-8">
+            <form onSubmit={handleLogin} className="card-body m-8">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text font-bold">Email</span>
@@ -46,8 +72,22 @@ const Login = () => {
                   className="input input-bordered"
                 />
               </div>
+              <div className="form-control">
+                <label className="label">
+                <LoadCanvasTemplate />
+                </label>
+                <input
+                  type="text"
+                  ref={captchaRef}
+                  placeholder="Type the text above"
+                  name="captcha"
+                  className="input input-bordered"
+                />
+                <button onClick={handleCaptcha} className="btn btn-xs btn-primary my-2">Validate Captcha</button>
+              </div>
               <div className="form-control mt-6">
-                <button className="btn bg-[#D1A054]">sign In</button>
+                
+                <input type="submit" disabled={disable}  className="btn bg-[#D1A054]" value="Sign In" />
               </div>
               <p className="text-center font-medium py-2">New to Bistro Boss?<Link to='/register' className="text-blue-700 font-bold"> Sign Up</Link></p>
               <div>
@@ -58,7 +98,7 @@ const Login = () => {
                     <button className="p-1 m-2 btn btn-circle bg-gray-300 hover:bg-[#D1A054] text-gray-900 text-2xl"><FaGithub/></button>
                 </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
