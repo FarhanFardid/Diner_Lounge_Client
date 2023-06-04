4b1aa94c106e9078bf632612ae40a19c7b4039aa
FarhanFardid <farhanfardid009@gmail.com>
2023-06-04 17:24:56 +0600

    admin item add page functional

--- src/Pages/DashBoard/Admin/AdminAddItem.jsx
+++ src/Pages/DashBoard/Admin/AdminAddItem.jsx
@@ -1,7 +1,50 @@
 import { ImSpoonKnife } from "react-icons/im";
 import SectionTitle from "../../../Shared/SectionTitle";
+import { useForm } from "react-hook-form";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
+const image_Hosting_Token = import.meta.env.VITE_Image_Upload_Token;
 const AdminAddItem = () => {
+    const [axiosSecure] = useAxiosSecure();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const image_Hosting_Url = `https://api.imgbb.com/1/upload?key=${image_Hosting_Token}`
+
+  const onSubmit = data => {
+
+    const formData = new FormData();
+    formData.append('image', data.image[0])
+    fetch(image_Hosting_Url, {
+        method: "POST",
+        body: formData
+    })
+    .then(res =>res.json())
+    .then (imgResponse =>{
+        console.log(imgResponse)
+        if(imgResponse.success){
+            const imgURL = imgResponse.data.display_url;
+            const {name,price,category,recipe} = data;
+            const newItem = {name, price:parseFloat(price), category, recipe, image:imgURL}
+            console.log(newItem); 
+            axiosSecure.post('/menu', newItem)
+            .then(data=>{
+                console.log(data.data)
+                if (data.data.insertedId) {
+                    
+                    Swal.fire({
+                      title: "Success",
+                      text: "New Recipe Added to Menu Successfully ",
+                      icon: "success",
+                      confirmButtonText: "Cool",
+                    });
+                  } 
+            })
+
+        }
+    })
+    console.log(data);
+  }
+  console.log(image_Hosting_Token)
   return (
     <div>
       <SectionTitle
@@ -9,7 +52,7 @@ const AdminAddItem = () => {
         subHeading="What's New "
       ></SectionTitle>
       <div className="p-4">
-        <form className="grid grid-cols-2 gap-5 m-5 bg-slate-200 rounded-lg" >
+        <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-2 gap-5 m-5 bg-slate-200 rounded-lg" >
           <div className="form-control w-full col-span-2 px-3 ">
             <label className="label">
               <span className="label-text font-bold">Recipe Name*</span>
@@ -17,23 +60,28 @@ const AdminAddItem = () => {
             <input
               type="text"
               placeholder="Enter recipe name"
+              {...register("name", { required: true })}
               className="input input-bordered w-full"
             />
+               {errors.name && <span className="text-red-700">Recipe name is required</span>}
              </div>
             <div className="form-control w-full px-3 ">
   <label className="label">
     <span className="label-text  font-bold">Category*</span>
 
   </label>
-  <select className="select select-bordered">
-    <option disabled selected>Category</option>
+  <select defaultValue="category" className="select select-bordered" {...register("category", { required: true })}>
+ 
+    <option disabled >Category</option>
     <option>offered</option>
     <option>popular</option>
     <option>pizza</option>
     <option>dessert</option>
     <option>salad</option> 
     <option>soup</option> 
+    {errors.category && <span className="text-red-700">Category is required</span>}
   </select>
+  
  
 </div>
          
@@ -43,28 +91,32 @@ const AdminAddItem = () => {
               <span className="label-text font-bold">Price*</span>
             </label>
             <input
-              type="text"
+              type="number"
               placeholder="Enter price"
+              {...register("price", { required: true })}
               className="input input-bordered w-full "
             />
+            {errors.price && <span className="text-red-700">Price is required</span>}
           </div>
           <div className="form-control col-span-2 px-3">
   <label className="label">
     <span className="label-text  font-bold">Recipe Details*</span>
   </label>
-  <textarea className="textarea textarea-bordered h-24" placeholder="Enter recipe details"></textarea>
-  
+  <textarea className="textarea textarea-bordered h-24" placeholder="Enter recipe details" {...register("recipe", { required: true })}></textarea>
+  {errors.recipe && <span className="text-red-700">Recipe details is required</span>}
 </div>
 <div className="form-control w-full px-3 py-1">
   <label className="label">
     <span className="label-text font-bold">Upload an image*</span>
   
   </label>
-  <input type="file" className="file-input file-input-bordered w-full" />
+  <input type="file" className="file-input file-input-bordered w-full" {...register("image", { required: true })} />
+  {errors.image && <span className="text-red-700">Recipe image is required</span>}
 
 </div>
 
-<div className="col-span-2 p-3 w-fit mx-auto"> <button className="btn bg-slate-900">Add Item <ImSpoonKnife className="h-5 w-5 m-1"/> </button></div>
+<div className="col-span-2 p-3 bg-slate-900 w-fit mx-auto flex text-white rounded-lg mb-5 hover:bg-slate-700">
+<input type="submit" className=" " value="Add Item" /> <i> <ImSpoonKnife className="h-5 w-5"/></i></div>
         </form>
       </div>
     </div>
