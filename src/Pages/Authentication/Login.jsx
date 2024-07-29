e26ced605576f60287028f6f94fed242c323abdb
FarhanFardid <farhanfardid009@gmail.com>
2024-07-29 16:52:59 +0600

    Updates

--- src/Pages/Authentication/Login.jsx
+++ src/Pages/Authentication/Login.jsx
@@ -1,139 +1,132 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import loginBanner from "../../assets/others/authentication.gif";
 import authImg from "../../assets/others/authentication1.png";
-import { FaGoogle,FaFacebook,FaGithub } from 'react-icons/fa';
-import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { FaGoogle, FaFacebook, FaGithub } from "react-icons/fa";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  validateCaptcha,
+} from "react-simple-captcha";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Helmet } from "react-helmet-async";
 
 const Login = () => {
-  const { userSignIn,google,github} = useContext(AuthContext);
+  const { userSignIn, google, github } = useContext(AuthContext);
 
-    const location = useLocation();
-    const navigate = useNavigate();
-    const from = location.state?.from?.pathname || '/'
-    const [disable,setDisable] = useState(true)
-    useEffect(()=>{
-        loadCaptchaEnginge(6);
-    },[])
-    const handleCaptcha = (e)=>{
-        const user_captcha_value = e.target.value;
-        // console.log(user_captcha_value);
-        if (validateCaptcha(user_captcha_value)){
-           setDisable(false)
-        }
-        else{
-            setDisable(true)
-        }
-         }
-         const handleLogin= event =>{
-            event.preventDefault();
-            const form = event.target;
-            const email = form.email.value;
-            const password = form.password.value;
-            console.log(email,password);
-            userSignIn(email,password)
-            .then(res=>{
-              const loggedUser = res.user;
-              toast.success("Successfully Signed In")
-              // console.log(loggedUser);
-              navigate(from, {replace:true})
-              form.reset()
-               const saveUser = {
-                name: loggedUser.displayName
-                  ? loggedUser.displayName
-                  : "Name not available",
-                email: loggedUser.email,
-              };
-              // console.log(saveUser);
-              // fetch("http://localhost:5000/users", {
-              fetch("https://diner-lounge-server.vercel.app/users", {
-                method: "POST",
-                headers: {
-                  "content-type": "application/json",
-                },
-                body: JSON.stringify(saveUser),
-              })
-                .then((res) => res.json())
-                .then((data) => {
-                  if (data.insertedId) {
-                    toast.success("User Successfully Added to DB ");
-              
-                    
-                  }
-                })
-            } )
-              
-            
-            
-            .catch(error=> {
-              console.log(error);
-              toast.error("Sign In Failed")
-            })
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || "/";
+  const [disable, setDisable] = useState(true);
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
+  const handleCaptcha = (e) => {
+    const user_captcha_value = e.target.value;
+    // console.log(user_captcha_value);
+    if (validateCaptcha(user_captcha_value)) {
+      setDisable(false);
+    } else {
+      setDisable(true);
+    }
+  };
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+    userSignIn(email, password)
+      .then((res) => {
+        const loggedUser = res.user;
+        toast.success("Successfully Signed In");
+        // console.log(loggedUser);
+        navigate(from, { replace: true });
+        form.reset();
+        const saveUser = {
+          name: loggedUser.displayName
+            ? loggedUser.displayName
+            : "Name not available",
+          email: loggedUser.email,
+        };
+        // console.log(saveUser);
+        // fetch("http://localhost:5000/users", {
+        fetch("https://diner-lounge-server.vercel.app/users", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(saveUser),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.insertedId) {
+              toast.success("User Successfully Added to DB ");
+            }
+          });
+      })
 
-         }
-         const googleHandle = () =>{
-          google()
-          .then(res =>{
-            const loggedUser = res.user; 
-            // console.log(loggedUser);
-            toast.success("Successfully Signed In")
-            navigate(from, {replace:true})
-            const saveUser = {
-              name: loggedUser.displayName
-                ? loggedUser.displayName
-                : "Name not available",
-              email: loggedUser.email,
-            };
-            // console.log(saveUser);
-            // fetch("http://localhost:5000/users", {
-            fetch("https://diner-lounge-server.vercel.app/users", {
-              method: "POST",
-              headers: {
-                "content-type": "application/json",
-              },
-              body: JSON.stringify(saveUser),
-            })
-              .then((res) => res.json())
-              .then((data) => {
-                if (data.insertedId) {
-                  toast.success("User Successfully Added to DB ");
-            
-                  
-                }
-              })
-          } )
-          .catch(error=> {
-            console.log(error);
-            toast.error("Sign In Failed")
-          })
-        }
-          const githubHandle = () =>{
-            github()
-            .then(res =>{
-              const loggedUser = res.user; 
-              toast.success("Successfully Signed In")
-              navigate(from, {replace:true})
-              console.log(loggedUser);
-            } )
-            .catch(error=> {
-              console.log(error);
-              toast.error("Sign In Failed")
-            })
-           
-         
-        }
-        const fbHandle = () =>{
-          toast.warning("Facebook Integration will be implemented soon..... ")
-         }
+      .catch((error) => {
+        console.log(error);
+        toast.error("Sign In Failed");
+      });
+  };
+  const googleHandle = () => {
+    google()
+      .then((res) => {
+        const loggedUser = res.user;
+        // console.log(loggedUser);
+        toast.success("Successfully Signed In");
+        navigate(from, { replace: true });
+        const saveUser = {
+          name: loggedUser.displayName
+            ? loggedUser.displayName
+            : "Name not available",
+          email: loggedUser.email,
+        };
+        // console.log(saveUser);
+        // fetch("http://localhost:5000/users", {
+        fetch("https://diner-lounge-server.vercel.app/users", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(saveUser),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.insertedId) {
+              toast.success("User Successfully Added to DB ");
+            }
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Sign In Failed");
+      });
+  };
+  const githubHandle = () => {
+    github()
+      .then((res) => {
+        const loggedUser = res.user;
+        toast.success("Successfully Signed In");
+        navigate(from, { replace: true });
+        console.log(loggedUser);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Sign In Failed");
+      });
+  };
+  const fbHandle = () => {
+    toast.warning("Facebook Integration will be implemented soon..... ");
+  };
   return (
     <div>
       <Helmet>
         <title>Diner Lounge | Login</title>
-       
       </Helmet>
       <div
         className="hero h-[550px]"
@@ -179,32 +172,55 @@ const Login = () => {
               </div>
               <div className="form-control">
                 <label className="label">
-                <LoadCanvasTemplate />
+                  <LoadCanvasTemplate />
                 </label>
                 <input
                   type="text"
-                 onBlur={handleCaptcha}
+                  onBlur={handleCaptcha}
                   placeholder="Type the text above"
                   name="captcha"
                   className="input input-bordered"
                 />
-              
               </div>
               <div className="form-control mt-6">
-                
-                <input type="submit" disabled={disable}  className="btn bg-slate-700 hover:bg-slate-900" value="Sign In" />
+                <input
+                  type="submit"
+                  disabled={disable}
+                  className="btn bg-slate-700 hover:bg-slate-900"
+                  value="Sign In"
+                />
               </div>
-              </form>
-              <p className="text-center font-medium py-2">New to Diner Lounge?<Link to='/register' className="text-blue-700 font-bold"> Sign Up</Link></p>
-              <div>
-                <p className="font-bold text-lg text-center">Or Sign In with</p>
-                <div className="flex justify-center py-3">
-                    <button onClick={fbHandle} className="p-1 m-2 btn btn-circle  bg-gray-600 hover:bg-slate-800 text-white text-2xl"><FaFacebook/></button>
-                    <button onClick={googleHandle} className="p-1 m-2 btn btn-circle  bg-gray-600 hover:bg-slate-800 text-white text-2xl"><FaGoogle/></button>
-                    <button onClick={githubHandle} className="p-1 m-2 btn btn-circle  bg-gray-600 hover:bg-slate-800 text-white text-2xl"><FaGithub/></button>
-                </div>
+            </form>
+            <p className="text-center font-medium py-2">
+              New to Diner Lounge?
+              <Link to="/register" className="text-blue-700 font-bold">
+                {" "}
+                Sign Up
+              </Link>
+            </p>
+            <div>
+              <p className="font-bold text-lg text-center">Or Sign In with</p>
+              <div className="flex justify-center py-3">
+                <button
+                  onClick={fbHandle}
+                  className="p-1 m-2 btn btn-circle  bg-gray-600 hover:bg-slate-800 text-white text-2xl"
+                >
+                  <FaFacebook />
+                </button>
+                <button
+                  onClick={googleHandle}
+                  className="p-1 m-2 btn btn-circle  bg-gray-600 hover:bg-slate-800 text-white text-2xl"
+                >
+                  <FaGoogle />
+                </button>
+                <button
+                  onClick={githubHandle}
+                  className="p-1 m-2 btn btn-circle  bg-gray-600 hover:bg-slate-800 text-white text-2xl"
+                >
+                  <FaGithub />
+                </button>
               </div>
-          
+            </div>
           </div>
         </div>
       </div>
--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,95 +1,98 @@
-import { createContext, useEffect, useState} from "react";
-import {  GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import app from "../Firebase/firebase.config";
 import axios from "axios";
 
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
- export const AuthContext = createContext(null); 
-const AuthProvider = ({children}) => {
-const[user,setUser] = useState(null);
-const[loading,setLoading] = useState(true);
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+  // const fbProvider = new FacebookAuthProvider();
 
-    const auth = getAuth(app);
-    const googleProvider = new GoogleAuthProvider();
-    const githubProvider = new GithubAuthProvider();
-    // const fbProvider = new FacebookAuthProvider();
+  const google = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const userSignIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const userUpdate = (user, name, photo) => {
+    setLoading(true);
+    return updateProfile(user, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
 
-    const google=()=>{
-        setLoading(true)
-        return signInWithPopup(auth,googleProvider)
+  const github = () => {
+    setLoading(true);
+    return signInWithPopup(auth, githubProvider);
+  };
+  // const facebook=()=>{
+  //     setLoading(false)
+  //     return signInWithPopup(auth,fbProvider)
+  // }
 
-    }
-    const createUser=(email,password)=>
-    {
-        setLoading(true)
-     return createUserWithEmailAndPassword(auth,email,password)
+  const logOut = () => {
+    return signOut(auth);
+  };
 
-    }
-    const userSignIn =(email,password) =>{
-        setLoading(true)
-        return signInWithEmailAndPassword(auth,email,password)
-    } 
-    const userUpdate = (user,name,photo)=>{
-        setLoading(true)
-       return updateProfile(user, {
-            displayName: name , photoURL: photo
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        // axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+        axios
+          .post("https://diner-lounge-server.vercel.app/jwt", {
+            email: currentUser.email,
           })
-    }
-   
-    const github=() =>{
-        setLoading(true)
-        return signInWithPopup(auth,githubProvider)
-    }
-    // const facebook=()=>{
-    //     setLoading(false)
-    //     return signInWithPopup(auth,fbProvider)
-    // }
+          .then((data) => {
+            // console.log(data.data.token);
+            localStorage.setItem("access-token", data.data.token);
+            setLoading(false);
+          });
+      } else {
+        localStorage.removeItem("access-token");
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [auth]);
 
-    const logOut = () =>{
-        return signOut(auth)
-    }
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    userSignIn,
+    logOut,
+    userUpdate,
+    google,
+    github,
+  };
 
-    useEffect(()=>{
-        const unsubscribe = onAuthStateChanged(auth,currentUser=>{
-            setUser(currentUser);
-            if(currentUser){
-                // axios.post('http://localhost:5000/jwt', {email: currentUser.email})
-                axios.post('https://diner-lounge-server.vercel.app/jwt', {email: currentUser.email})
-                .then (data =>{
-                    // console.log(data.data.token);
-                    localStorage.setItem('access-token', data.data.token)
-                    setLoading(false);
-                })
-            }
-            else{
-                localStorage.removeItem('access-token')
-            }
-            
-    
-        });
-        return ()=>{
-            unsubscribe();
-        }
-       },[auth])
-       
-    const authInfo={
-        user,
-        loading,
-        createUser,
-        userSignIn,
-        logOut,
-        userUpdate,
-        google,
-        github,
-
-    }
-
-
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
